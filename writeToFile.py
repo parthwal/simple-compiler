250afcf5f3aaa9d315ed6eb5f5b3bb2b136470e0
Parth Barthwal <102653990+parthwal@users.noreply.github.com>
2022-07-06 23:47:49 +0530

    Delete writeToFile.py

--- writeToFile.py
+++
@@ -1,7 +0,0 @@
-def input(): #input instructions
-    val=""
-    with open("instructions.txt","w") as wTF:
-        while(val.lower()!="hlt"):
-            val=input().lower()
-            wTF.write(val)
-            wTF.write("\n")
