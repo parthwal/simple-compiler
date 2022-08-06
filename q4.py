b6e3e56be32175a3e4d443b667c67c6a7b17f298
RishabKumar21087 <johndoe@example.com>
2022-08-06 22:45:46 +0530

    Q4 PARTIALLY DONE

--- q4.py
+++ q4.py
@@ -1,5 +1,21 @@
 import matplotlib.pyplot as plt
 
 # q4 utilities
+'''
+time = 0
+after every exec time increments such as after retriving mem executing etc (exeption dumping and upgrading pc)
 
-mem_adress= []
+'''
+mem_adress = []
+f = open("mem.txt")
+pcode = f.read().strip().split("\n")
+p_code = [i.split() for i in pcode]
+x = []
+y = []
+for i in p_code:
+    x.append(i[0])
+    y.append(i[1])
+
+plt.scatter(x, y)
+plt.show()
+
